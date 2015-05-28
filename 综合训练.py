c1503179354c8a34be943958c94dbffe98ffee01
Marsjinzong <418246299@qq.com>
2015-05-28 22:40:06 +0800

    综合训练改动后再次提交

--- 综合训练.py
+++ 综合训练.py
@@ -1,32 +1,37 @@
-import pickle
- 
-pickle file = open('data.pkl', 'rb')
-city = pickle.load(pickle_file)
- 
-str = input('请输入身份证号码：')
-cid = str[:6]
-birth = str[6:14]
-cname = city[cid]
- 
-num = int(str[1])
-tmpsum = int(str[0])*7 + int(str[1])*9 + int(str[2])*10 + int(str[3])*5 + int(str[4])*8 \
-        + int(str[5])*4 + int(str[6])*2 + int(str[7])*1 + int(str[8])*6 + int(str[9])*3 \
-        + int(str[10])*7 + int(str[11])*9 + int(str[12])*10 + int(str[13])*5 \
-        + int(str[14])*8 + int(str[15])*4 + int(str[16])*2;
- 
-remainder = tmpsum % 11
- 
-sex = int(str[16]) % 2
-if sex == 1:
-    sex = '男'
-else :
-    sex = '女'
- 
-maptable = {0: '1', 1: '0', 2: 'x', 3: '9', 4: '8', 5: '7', 6: '6', 7: '5', 8: '4', 9: '3', 10: '2'}
-if maptable[remainder] == str[17]:
-    print('<身份证合法>')
-    print('性别：' + sex)
-    print('出生日期:' + birth)
-    print('归属地:' + cname)
-else :
-    print('<身份证不合法>')
+
+#-*-coding:utf8;-*-
+#qpy:2
+#qpy:console
+
+print "This is console module"
+def checkid(idnum):
+ a={i:(2**(18-i))%11 for i in range(1,18)}
+ end ={0:"1",1:"0",2:"X",3:"9",4:"8",5:"7",6:"6",7:"5",8:"4",9:"3",10:"2"}
+ if len(idnum)!=18:
+  return "wrong number"
+ else: 
+  sum =0
+  for i, j in enumerate(idnum,start =1):
+   if i==18 and end[sum%11]==j:
+	return "True"
+   elif i==18 and end[sum%11]!=j:
+    return  "False"
+   else: 
+    sum=sum +int(j)*a[i]
+idnum  =raw_input("input id: ")
+print checkid(idnum)
+print u'以下是随机生成的身份证号进行验证：'
+import random
+ranid=""
+for i in range(18):
+ if i==17:
+  ranid += str(random.choice(range(10)+["X"]))
+ else: 
+  ranid +=str(random.randint(0,9))
+print ranid
+print checkid(ranid)
+print u'以下是从文件中读取到的身份证号并进行验证，验证结果如下：'
+file = open('C:\Users\Lenovo\Desktop\id_number.txt','r')
+for val in file:
+	idnum = str(val)
+	print checkid(idnum)
